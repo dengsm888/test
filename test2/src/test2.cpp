a39c7c18b11193e0e983b3a9924c5139dee37b15
dengsm888 <deng.shiming@qq.com>
2022-06-09 20:57:20 +0800

    initial state

---
+++ test2/src/test2.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : test2.cpp
+// Author      : DSM
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello kill World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
